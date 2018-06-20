212e3c595a89abb438bacf51012039b2ae914283
FIC <FIC@lab_08_ws04.info.local>
2018-06-20 19:28:54 -0300

    Adicionar arquivos de projeto.

---
+++ Senai.Chamados.Web/Models/CadastrarUsuarioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Senai.Chamados.Web.Models
+{
+    public class CadastrarUsuarioViewModel
+    {
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public String Telefone { get; set; }
+        public String Senha { get; set; }
+
+    }
+}
