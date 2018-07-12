7ca3c38fb4dbfc5f63c74ef556728efd93b7e130
Leonardo Tarrou <tarroung@gmail.com>
2018-07-11 22:01:55 -0300

    Graficos Setor e Status

--- Senai.Chamados.Web/Controllers/GraficosController.cs
+++ Senai.Chamados.Web/Controllers/GraficosController.cs
@@ -3,6 +3,7 @@ using Senai.Chamados.Data.Repositorios;
 using Senai.Chamados.Domain.Entidades;
 using Senai.Chamados.Domain.Enum;
 using Senai.Chamados.Web.ViewModels.Chamados;
+using Senai.Chamados.Web.ViewModels.Grafico;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,17 @@ using System.Web.Mvc;
 
 namespace Senai.Chamados.Web.Controllers
 {
-    [Authorize]
+ 
     public class GraficosController : Controller
     {
+
+        [Authorize]
         // GET: Graficos
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public JsonResult GetDataStatus()
-        {
             try
             {
-
+                ListaGraficoViewModel vmGrafico = new ListaGraficoViewModel();
                 ListaChamadoViewModel vmListaChamados = new ListaChamadoViewModel();
 
                 using (ChamadoRepositorio _repChamado = new ChamadoRepositorio())
@@ -44,33 +42,84 @@ namespace Senai.Chamados.Web.Controllers
                     }
 
                 }
+
+                #region Grafico Status
+                // Faz o agrupamento dos dados por status
                 var grupoStatus = vmListaChamados.ListaChamados
                     .GroupBy(x => x.Status)
                     .Select(n => new
                     {
-                        Status = (EnStatus)n.Key,
-                        Quantidade = n.Count()
+                        Status = RetornaStatus(n.Key),
+                        Quantidade = Convert.ToDouble(n.Count())
                     }).OrderBy(n => n.Quantidade);
 
-                return Json(new { sucesso = true, resultado = grupoStatus }, JsonRequestBehavior.AllowGet);     
+                // Atribui as labels que serão mostradas no gráfico
+                vmGrafico.GraficoStatus.Labels = grupoStatus.Select(x => x.Status).ToArray();
 
-           }
+                // Atribui os dados que serão apresentadas no grafico
+                vmGrafico.GraficoStatus.Data = grupoStatus.Select(x => x.Quantidade).ToArray();
+                #endregion
+
+
+                #region Grafico Setor
+                // Faz o agrupamento dos dados por status
+                var grupoSetor = vmListaChamados.ListaChamados
+                    .GroupBy(x => x.Setor)
+                    .Select(n => new
+                    {
+                        Setor = RetornaSetor(n.Key),
+                        Quantidade = Convert.ToDouble(n.Count())
+                    }).OrderBy(n => n.Quantidade);
+
+                // Atribui as labels que serão mostradas no gráfico
+                vmGrafico.GraficoSetor.Labels = grupoSetor.Select(x => x.Setor).ToArray();
+
+                // Atribui os dados que serão apresentadas no grafico
+                vmGrafico.GraficoSetor.Data = grupoSetor.Select(x => x.Quantidade).ToArray();
+                #endregion
+
+                return View(vmGrafico);
+            }
             catch (Exception ex)
             {
-
                 ViewBag.Erro = ex.Message;
-                return Json(new
-                {
-                    sucesso = false,
-                    message = ex.Message
-
-                }, JsonRequestBehavior.AllowGet);
-                
-            }
+                return View();
+            }           
             
         }
 
+        private string RetornaStatus(EnStatus status)
+        {
+            switch (status)
+            {
+                case EnStatus.Aguardando:
+                    return "Aguardando";
+                case EnStatus.Iniciado:
+                    return "Iniciado";
+                    
+                case EnStatus.Finalizado:
+                    return "Finalizado";                
+            }
+            return null;
+        }
 
+
+        private string RetornaSetor(EnSetor setor)
+        {
+            switch (setor)
+            {
+                case EnSetor.Administrativo:
+                    return "Administrativo";
+                case EnSetor.Informatica:
+                    return "Informática";
+
+                case EnSetor.Recepcao:
+                    return "Receção";
+            }
+            return null;
+        }
 
     }
+
+
 }
